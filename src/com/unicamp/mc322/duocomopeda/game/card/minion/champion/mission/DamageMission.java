2dfc857b4f0d8f48b43b9fd6bf83fc78507e0038
erickmana <e215448@dac.unicamp.br>
2021-06-29 20:47:46 -0300

    create mission type files

---
+++ src/com/unicamp/mc322/duocomopeda/game/card/minion/champion/mission/DamageMission.java
@@ -0,0 +1,12 @@
+package com.unicamp.mc322.duocomopeda.game.card.minion.champion.mission;
+
+import com.unicamp.mc322.duocomopeda.game.card.effect.EffectTrigger;
+
+public abstract class DamageMission extends Mission {
+
+    public DamageMission(int goal, EffectTrigger trigger) {
+        super(goal, trigger);
+        // TODO Auto-generated constructor stub
+    }
+
+}
