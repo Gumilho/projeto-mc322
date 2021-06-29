334bcc9ed6bc22135b12eee28ca46c7fa580ac93
Gumilho <doraniel2000@gmail.com>
2021-06-29 18:25:22 -0300

    Set test parameters

--- src/com/unicamp/mc322/duocomopeda/main/Tester.java
+++ src/com/unicamp/mc322/duocomopeda/main/Tester.java
@@ -1,8 +1,11 @@
 package com.unicamp.mc322.duocomopeda.main;
 
+import com.unicamp.mc322.duocomopeda.game.Game;
 public class Tester {
 
-    public Tester() {
+    public static void main(String[] args) {
+        Game game = Game.getInstance("joao", "maria");
+        game.startGame();
     }
 
 }
