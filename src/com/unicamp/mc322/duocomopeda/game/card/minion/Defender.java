b74bbe65cde97cbc198d45ad700b2aaf183ff510
Gumilho <doraniel2000@gmail.com>
2021-07-06 02:23:46 -0300

    new Card Defender

---
+++ src/com/unicamp/mc322/duocomopeda/game/card/minion/Defender.java
@@ -0,0 +1,23 @@
+package com.unicamp.mc322.duocomopeda.game.card.minion;
+
+import java.util.EnumSet;
+
+import com.unicamp.mc322.duocomopeda.game.player.Player;
+
+public class Defender extends Minion {
+    
+    public Defender(Player owner) {
+        super(
+            "Defender", 
+            2, 2, 2, 
+            EnumSet.of(Trait.FURY),
+            owner,
+            "Trait: Fury (+0/+1)"
+        );
+    }
+    
+    @Override
+    public void onKill(Player owner, Minion killer, Minion killed) {
+        this.buff(0, 1);
+    }
+}
